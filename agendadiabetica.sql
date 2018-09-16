bdbd1ddfa42341ea49bbaa00edd9c94fd24c25a5
MagonxESP <janma.360@gmail.com>
2018-09-16 11:36:12 +0200

    foreign key

--- agendadiabetica.sql
+++ agendadiabetica.sql
@@ -1,13 +1,19 @@
-USE qaan853;
+CREATE DATABASE IF NOT EXISTS glucosa CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';
+
+USE glucosa;
+
+DROP TABLE IF EXISTS glucosa;
+DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	usuario varchar (40) UNIQUE,
 	contrasenia varchar(40)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE glucosa
 (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	fecha DATE not NULL,
     manyana INT NOT NULL,
     insulinam INT NOT NULL,
@@ -18,11 +24,10 @@ CREATE TABLE glucosa
     cena INT NOT NULL,
 	insulinace INT NOT NULL,
     insulinalenta INT  NOT NULL,
-    dormir INT  NOT NULL,
-    observaciones varchar (255)
- 
- 
- 
-);
-
-DROP TABLE glucosa;
+    dormir INT NOT NULL,
+    observaciones varchar(255),
+	usuario INT NOT NULL,
+    FOREIGN KEY (usuario) REFERENCES usuarios(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+)ENGINE=InnoDB;
